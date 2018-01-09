5534547e012bd7d6eb27c9f596116ad3921238ee
bizelite <bizelite@naver.com>
2018-01-09 20:42:52 +0900

    Data Sort Algorithm : Selection Sort

--- psaa/selection_sort.py
+++ psaa/selection_sort.py
@@ -2,17 +2,18 @@
 
 def selection_sort(lst):
     for i in range(len(lst)-1):
-        min_position = i
+        min_pos = i
         for j in range(i+1, len(lst)):
-            if lst[j] < lst[min_position]:
-                min_position = j
+            if lst[j] < lst[min_pos]:
+                min_pos = j
 
-        if min_position != i:
+        if min_pos != i:
             tmp = lst[i]
-            lst[i] = lst[min_position]
-            lst[min_position] = tmp
+            lst[i] = lst[min_pos]
+            lst[min_pos] = tmp
+
 
 if __name__ == '__main__':
     lst = [8, 4, 7, 3, 5, 2, 9]
     selection_sort(lst)
-    
+    print(lst)
