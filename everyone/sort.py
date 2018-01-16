1e82c74dedc9a43f611f7c17bd43cfed4a673b00
bizelite <bizelite@naver.com>
2018-01-16 21:58:08 +0900

    Sorting Algorithm List

--- everyone/sort.py
+++ everyone/sort.py
@@ -18,6 +18,64 @@ def insert_sort(l):
         l[j+1] = key
 
 
+def merge_sort(l):
+    n = len(l)
+    if n <= 1:
+        return
+
+    mid = n // 2
+    g1 = l[:mid]
+    g2 = l[mid:]
+
+    merge_sort(g1)
+    merge_sort(g2)
+
+    i1 = 0
+    i2 = 0
+    il = 0
+
+    while i1 < len(g1) and i2 < len(g2):
+        if g1[i1] < g2[i2]:
+            l[il] = g1[i1]
+            i1 += 1
+            il += 1
+        else:
+            l[il] = g2[i2]
+            i2 += 1
+            il += 1
+    
+    while i1 < len(g1):
+        l[il] = g1[i1]
+        i1 += 1
+        il += 1
+
+    while i2 < len(g2):
+        l[il] = g2[i2]
+        i2 += 1
+        il += 1
+
+
+def quick_sort_sub(l, start, end):
+    if end - start <= 0:
+        return
+
+    pivot = l[end]
+    i = start
+
+    for j in range(start, end):
+        if l[j] <= pivot:
+            l[i], l[j] = l[j], l[i]
+            i += 1
+    l[i], l[end] = l[end], l[i]
+
+    quick_sort_sub(l, start, i-1)
+    quick_sort_sub(l, i+1, end)
+
+
+def quick_sort(l):
+    quick_sort_sub(l, 0, len(l)-1)
+
+
 
 if __name__ == '__main__':
     v = [17, 92, 18, 33, 58, 7, 18, 33, 42]
@@ -25,3 +83,8 @@ if __name__ == '__main__':
     print('Selection Sort:', selection_sort(v))
     insert_sort(v)
     print('Insert Sort:', v)
+    merge_sort(v)
+    print('Merge Sort:', v)
+    quick_sort(v)
+    print('Quick Sort:', v)
+
