208757eaabcfec571a18a0da65731546623c4f35
bizelite <bizelite@naver.com>
2018-01-16 21:34:58 +0900

    Added Insert Sort Algorithm

--- everyone/sort.py
+++ everyone/sort.py
@@ -8,7 +8,20 @@ def selection_sort(l):
     return l
 
 
+def insert_sort(l):
+    for i in range(1, len(l)):
+        key = l[i]
+        j = i - 1
+        while j >= 0 and l[j] > key:
+            l[j+1] = l[j]
+            j -= 1
+        l[j+1] = key
+
+
+
 if __name__ == '__main__':
     v = [17, 92, 18, 33, 58, 7, 18, 33, 42]
 
-    print(selection_sort(v))
+    print('Selection Sort:', selection_sort(v))
+    insert_sort(v)
+    print('Insert Sort:', v)
