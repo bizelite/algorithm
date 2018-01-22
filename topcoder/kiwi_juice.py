c54cf79b8390c2c9294c89ec3ce4c0b6f2d85b25
bizelite <bizelite@naver.com>
2018-01-22 23:29:45 +0900

    Kiwi Juice Problem

---
+++ topcoder/kiwi_juice.py
@@ -0,0 +1,14 @@
+class KiwiJuice:
+    def __init__(self, capa, bottle, from_id, to_id):
+        self.capa = []
+        self.bottle = []
+        self.from_id = []
+        self.to_id = []
+
+    def pour(from_id, to_id):
+        pass
+
+
+
+if __name__ == '__main__':
+    pass
