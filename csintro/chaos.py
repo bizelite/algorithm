0ce9c3792dead60434ab95819be87e7d53aec1ac
bizelite <bizelite@naver.com>
2018-01-25 22:11:58 +0900

    Chaos Problem

--- csintro/chaos.py
+++ csintro/chaos.py
@@ -1,10 +1,18 @@
 def main():
     print('This program illustrates a chaotic function')
+    n = eval(input('Howw many numbers shuid I print? '))
     x = eval(input('Enter a number between 0 and 1: '))
-    for i in range(10):
-        x = 3.9 * x * (1- x)
-        print(x)
+    y = x
+    z = x
+
+    print('Case a)      Case b)     Case c)')
+    print('-------      -------     -------')
+    for i in range(n):
+        x = 3.9 * x * (1 - x)
+        y = 3.9 * (y - y * y)
+        z = 3.9 * z - 3.9 * z * z 
+        print("{0:>7f}     {1:>7f}     {2:>7f}".format(x, y, z))
 
 if __name__ == '__main__':
     main()
-    
+
