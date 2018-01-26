909435343f0d3fe07d99a3b71621a73c432d8a42
bizelite <bizelite@naver.com>
2018-01-26 21:31:06 +0900

    Future Value

--- csintro/futval.py
+++ csintro/futval.py
@@ -1,13 +1,27 @@
+def annuity(principal, rate, period, beginning=True):
+    total = 0
+    if beginning == True:
+        for i in range(1, period+1):
+            total = total + principal * (1 + rate)**i
+    else:
+        for i in range(1, period):
+            total = total + principal * (1 + rate)**i
+
+    return total
+
+
+
 def main():
     print('This program calculates the future value')
     print('of a n-year investment.')
 
     principal = eval(input('Enter the initial principal: '))
     apr = eval(input('Enter the annual interest rate: '))
+    periods = eval(input('Enter the periods per year: '))
     n = eval(input('Enter the period: '))
 
-    for i in range(n):
-        principal = principal * (1 + apr)
+    for i in range(n*periods):
+        principal = principal * (1 + apr/periods)
 
     print('The value in ', n, 'year is: ', principal)
 
