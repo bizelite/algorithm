99d134c7af287e51118ed3432006f2906fa2857d
bizelite <bizelite@naver.com>
2018-01-27 09:23:33 +0900

    Conversion

--- csintro/convert.py
+++ csintro/convert.py
@@ -2,9 +2,20 @@ def c2f(c):
     return 9/5*c + 32
 
 
+def f2c(f):
+    return 5/9*f - 160/9
+
+def k2m(k):
+    return 0.62 * k
+
 if __name__ == '__main__':
     print('Convert temperature from celcius to farenheit')
     for i in range(101):
         if i%10 == 0:
             print(i, c2f(i))
-    input('Press the <Enter> key to quit.')
+    
+    for i in range(212, 32, -1):
+        if i%10 == 0:
+            print(i, f2c(i))
+
+    input('Press <Enter> to quit.')
